0f8544a6f49a6269828d58d9e8f7edb0280af99d
jaehoon1022 <hyunsense1022@gmail.com>
2021-11-01 22:16:16 +0900

    Riot API TEST

---
+++ src/main/java/com/apitest/repository/SummonerRepository.java
@@ -0,0 +1,11 @@
+package com.apitest.repository;
+
+import com.apitest.dto.SummonerDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class SummonerRepository {
+}
